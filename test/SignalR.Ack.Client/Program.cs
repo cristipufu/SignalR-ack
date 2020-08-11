d65c45b1ec2c41e3b85086f34a05435781aa08ac
Cristi Pufu <cristian.pufu@uipath.com>
2020-08-11 17:17:30 +0300

    Initial commit

---
+++ test/SignalR.Ack.Client/Program.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignalR.Ack.Client
+{
+    class Program
+    {
+        static HubConnection connection;
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44343/DemoHub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            connection.Reconnecting += error =>
+            {
+                Debug.Assert(connection.State == HubConnectionState.Reconnecting);
+                Console.WriteLine("Reconnecting...");
+
+                // Notify users the connection was lost and the client is reconnecting.
+                // Start queuing or dropping messages.
+
+                return Task.CompletedTask;
+            };
+
+            connection.Reconnected += connectionId =>
+            {
+                Debug.Assert(connection.State == HubConnectionState.Connected);
+                Console.WriteLine("Reconnected.");
+
+                // Notify users the connection was reestablished.
+                // Start dequeuing messages queued while reconnecting if any.
+
+                return Task.CompletedTask;
+            };
+
+            connection.Closed += error =>
+            {
+                Debug.Assert(connection.State == HubConnectionState.Disconnected);
+                Console.WriteLine("Disconnected.");
+
+                // Notify users the connection has been closed or manually try to restart the connection.
+
+                return Task.CompletedTask;
+            };
+
+            connection.On<string>("ReceiveMessage", (message) =>
+            {
+                Console.WriteLine($"Received message: {message}");
+            });
+
+            await ConnectWithRetryAsync(connection, CancellationToken.None);
+
+            var i = 0;
+
+            while (true)
+            {
+                await connection.InvokeAsync("SendMessage", $"message-{i++}");
+
+                await Task.Delay(5000);
+            }
+        }
+
+        public static async Task<bool> ConnectWithRetryAsync(HubConnection connection, CancellationToken token)
+        {
+            // Keep trying to until we can start or the token is canceled.
+            while (true)
+            {
+                try
+                {
+                    await connection.StartAsync(token);
+                    Debug.Assert(connection.State == HubConnectionState.Connected);
+                    Console.WriteLine("Connected.");
+
+                    return true;
+                }
+                catch when (token.IsCancellationRequested)
+                {
+                    return false;
+                }
+                catch
+                {
+                    // Failed to connect, trying again in 5000 ms.
+                    Debug.Assert(connection.State == HubConnectionState.Disconnected);
+                    Console.WriteLine("Failed to connect, trying again in 5000 ms.");
+
+                    await Task.Delay(5000);
+                }
+            }
+        }
+    }
+}
---
+++ test/SignalR.Ack.Server/SignalR.Ack.Server/DemoHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalR.Ack.Server
+{
+    public class DemoHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "Topic");
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "Topic");
+
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public Task SendMessage(string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", $"Echo {message}");
+        }
+    }
+}
